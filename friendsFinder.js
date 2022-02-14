a788d7ed6c5121c7b9caa79f80fdf81c1dec6846
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-14 21:39:53 +0530

    fix: handle incorrect inputs

--- friendsFinder.js
+++ friendsFinder.js
@@ -1,8 +1,13 @@
 const users = require('./userData.json'); 
 function getFriendsName(username){
+    if (!(typeof username === 'string')){
+        throw new Error('Not a valid input type for username.');
+    }
     let friends = []
+    let flag = false;
     users.forEach((user) => {
         if(user.name === username){
+            flag = true;
             const userFriends = user.friends;
             userFriends.forEach((friend) => {
                 const {id,name} = friend;
@@ -10,6 +15,12 @@ function getFriendsName(username){
             })
         }
     });
-    return friends;
+    if(! flag){
+        throw new Error('User not exists.');
+    }
+    console.log(friends);
 }
-console.log(getFriendsName('Tisha Ryan'));
+
+getFriendsName('Tisha Ryan');
+
+module.exports = getFriendsName;
